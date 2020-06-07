860554113e2ba53e34c0af361aecf484e95e3f4c
maria legostina <legostinam@mail.ru>
2020-06-07 19:00:34 +0700

    Создает метки на карте

--- js/main.js
+++ js/main.js
@@ -5,70 +5,67 @@ var TYPES = ['place', 'flat', 'house', 'bungalo'];
 var TIMES = ['12:00', "13:00", "14:00"];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var PRICES_FLATS = {
+var DESCRIPTIONS = ['Окна выходят во двор, на запад. Напротив чудесный парк. В квартире светло, но не жарко. Прямые лучи солнца попадают в окна только вечером.', 'Близко к метро. В шаговой доступности торговые центры, бары и клубы.'];
+var PriceFlat = {
   MIN: 1000,
   MAX: 5000,
 }
-var ROOMS_FLATS = {
-  MIN: 1;
-  MAX: 4;
+var RoomFlat = {
+  MIN: 1,
+  MAX: 4,
 }
+var QuestFlat = {
+  MIN: 1,
+  MAX: 5,
+}
+var LocationVertical = {
+  MIN: 130,
+  MAX: 630,
+}
+var LocationHorizontal = {
+  MIN: 10,
+  MAX: 200,
+}
+var PIN_WIDTH = 40;
+var PIN_HEIGHT = 40;
+
+// var map = element.getBoundingClientRect();
 
 var getRandomInt = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-var getRandomPrices = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-var getRandomQuests = function (min, max) {
-  var min = (1);
-  var max = (4);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-var getRandomLocation = function (min, max) {
-  var min = (130);
-  var max = (630);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 var getRandomItem = function (items) {
-  var randomIndex = getRandomInt();
+  var randomIndex = getRandomInt(0, items.length);
   return items[randomIndex];
 }
-
-// Возвращает новый объект
 
 var getPin = function (index) {
   var pin = {
     author: {
-      avatar: 'img/avatars/user0' + index + '.png'
+      avatar: 'img/avatars/user0' + (++index) + '.png'
     } ,
     offer: {
       title: getRandomItem(TITLES),
       address: '600, 350',
-      price: getRandomPrices(PRICES_FLATS),
+      price: getRandomInt(PriceFlat.MIN, PriceFlat.MAX),
       type: getRandomItem(TYPES),
-      rooms: getRandomInt(ROOMS_FLATS),
-      guests: getRandomQuests(),
+      rooms: getRandomInt(RoomFlat.MIN, RoomFlat.MAX),
+      guests: getRandomInt(QuestFlat.MIN, QuestFlat.MAX),
       checkin: getRandomItem(TIMES),
       checkout: getRandomItem(TIMES),
       features: getRandomItem(FEATURES),
-      description: 'description',
+      description: getRandomItem(DESCRIPTIONS),
       photos: getRandomItem(PHOTOS),
     },
     location: {
-      x: getRandomInt(),
-      y: getRandomLocation()
+      x: getRandomInt(LocationHorizontal.MIN, LocationHorizontal.MAX),
+      y: getRandomInt(LocationVertical.MIN, LocationVertical.MAX)
     }
   };
 
   return pin;
 }
-
-// Возвращает массив
 
 var getPins = function (count) {
   var pins = [];
@@ -81,5 +78,30 @@ var getPins = function (count) {
   return pins;
 }
 
+var pinTemplate = document.querySelector('#pin');
+var mapPinsContainer = document.querySelector('.map__pins');
+
+var renderPin = function (pin) {
+  var pinButton = pinTemplate.content;
+  var buttonElement = pinButton.cloneNode(true);
+
+  buttonElement.querySelector('img').src = pin.author.avatar;
+  buttonElement.querySelector('img').alt = pin.offer.title;
+  buttonElement.querySelector('img').style.top = pin.location.x + PIN_WIDTH + 'px';
+  buttonElement.querySelector('img').style.left = pin.location.y + PIN_HEIGHT + 'px';
+
+  return buttonElement;
+}
+
+var renderPins = function(pins) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < pins.length; i++) {
+    fragment.appendChild(renderPin(pins[i]));
+  }
+  mapPinsContainer.appendChild(fragment);
+}
+
 var pins = getPins(8);
 console.log(pins);
+
+renderPins(pins);
