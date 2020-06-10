5367343942404c04748193f8718b4194ca26c671
maria legostina <legostinam@mail.ru>
2020-06-10 22:12:15 +0700

    Создает карточку объявления

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 'use strict';
 
+var PlacementType = {
+  PLACE: 'place',
+  FLAT: 'flat',
+  HOUSE: 'house',
+  BUNGALO: 'bungalo'
+};
+
+var DISPLAY_PLACEMENT_TYPE = {
+  [PlacementType.PLACE]: 'Дворец',
+  [PlacementType.FLAT]: 'Квартира',
+  [PlacementType.HOUSE]: 'Дом',
+  [PlacementType.BUNGALO]: 'Бунгало',
+};
+
 var TITLES = ['Уютная студия в черте города', 'Роскошные аппартаменты', 'Семейное гнездышко'];
-var TYPES = ['place', 'flat', 'house', 'bungalo'];
+var TYPES = [PlacementType.PLACE, PlacementType.FLAT, PlacementType.HOUSE, PlacementType.BUNGALO];
 var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var DESCRIPTIONS = ['Окна выходят во двор, на запад. Напротив чудесный парк. В квартире светло, но не жарко. Прямые лучи солнца попадают в окна только вечером.', 'Близко к метро. В шаговой доступности торговые центры, бары и клубы.'];
+
 var PriceFlat = {
   MIN: 1000,
   MAX: 5000,
@@ -28,6 +43,11 @@ var LocationHorizontal = {
 };
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+
+var pinButton = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapPinsContainer = document.querySelector('.map__pins');
+var mapCard = document.querySelector('#card').content.querySelector('.map__card');
+var mapContainer = document.querySelector('.map');
 
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -87,9 +107,6 @@ var getPins = function (count) {
   return pins;
 };
 
-var pinButton = document.querySelector('#pin').content.querySelector('.map__pin');
-var mapPinsContainer = document.querySelector('.map__pins');
-
 var renderPin = function (pin) {
   var buttonElement = pinButton.cloneNode(true);
 
@@ -110,6 +127,50 @@ var renderPins = function (pins) {
   mapPinsContainer.appendChild(fragment);
 };
 
+var renderPhotos = function (popupPhotosContainer, photos) {
+  var photoTemplate = popupPhotosContainer.querySelector('img');
+  var photoElement = popupPhotosContainer.cloneNode(true);
+  var fragment = document.createDocumentFragment();
+
+  photoTemplate.remove();
+
+  for (var i = 0; i < photos.length; i++) {
+    var photoElementNew = photoElement.cloneNode(true);
+
+    photoElementNew.src = photos[i];
+    fragment.appendChild(photoElementNew);
+  }
+
+  popupPhotosContainer.appendChild(fragment);
+}
+
+var renderPopup = function (pin) {
+  var popupElement = mapCard.cloneNode(true);
+
+  popupElement.querySelector('.popup__title').textContent = pin.offer.title;
+  popupElement.querySelector('.popup__text--address').textContent = pin.offer.address;
+  popupElement.querySelector('.popup__text--price').textContent = pin.offer.price + '₽/ночь';
+  popupElement.querySelector('.popup__type').textContent = DISPLAY_PLACEMENT_TYPE[pin.offer.type];
+  popupElement.querySelector('.popup__text--capacity').textContent = pin.offer.rooms + ' комнаты для ' + pin.offer.guests;
+  popupElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + pin.offer.checkin + ', выезд до ' + pin.offer.checkout;
+  popupElement.querySelector('.popup__features').textContent = pin.offer.features.join(',');
+  popupElement.querySelector('.popup__description').textContent = pin.offer.description;
+  popupElement.querySelector('.popup__avatar').src = pin.author.avatar;
+
+  var popupPhotosContainer = (popupElement.querySelector('.popup_photos'));
+
+  if (pin.offer.photos !== 0) {
+    renderPhotos(popupPhotosContainer, pin.offer.photos)
+  } else {
+    popupPhotosContainer.remove();
+  }
+
+  mapContainer.insertAdjacentHTML('afterbegin', popupElement);
+};
+
 var pins = getPins(8);
 
 renderPins(pins);
+renderPopup(pins[0]);
+
+console.log(pins);
