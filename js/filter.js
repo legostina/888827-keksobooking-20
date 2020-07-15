20a6599bb783e92612363c0f7b04449be203fcf2
maria legostina <legostinam@mail.ru>
2020-07-15 11:32:56 +0700

    Дорабатывает модуль с фильтрацией

--- js/filter.js
+++ js/filter.js
@@ -3,9 +3,36 @@
 (function () {
   var formContainer = document.querySelector('.map__filters');
   var houseType = document.querySelector('#housing-type');
+  var houseRoom = document.querySelector('#housing-rooms');
+  var houseGuest = document.querySelector('#housing-guests');
+
+  var PlacementPrice = {
+    ANY: 'any',
+    MIDDLE: 'middle',
+    LOW: 'low',
+    HIGH: 'high'
+  };
+
+  var QuantityRooms = {
+    ANY: 'any',
+    ONE: 1,
+    TWO: 2,
+    THREE: 3
+  };
+
+  var QuantityGuest = {
+    ANY: 'any',
+    ONE: 2,
+    TWO: 1,
+    THREE: 0
+  };
+
+  var PriceScope = {
+    LOW: 10000,
+    HIGH: 50000
+  };
 
   var MAX_PINS_COUNT = 5;
-
   var pins = null;
 
   var initialize = function (items) {
@@ -18,6 +45,27 @@
     return houseType.value === window.util.placementType.ANY || pin.offer.type === houseType.value;
   };
 
+  var filterPinsByHouseRoom = function (pin) {
+    return houseRoom.value === QuantityRooms.ANY || pin.offer.rooms == houseRoom.value;
+  };
+
+  var filterPinsByHouseGuest = function (pin) {
+    return houseGuest.value === QuantityGuest.ANY || pin.offer.guests == houseGuest.value;
+  };
+
+  var filterPinsByHousePrice = function (pin) {
+    switch (pin) {
+      case PlacementPrice.LOW:
+        return pin.offer.price < PriceScope.LOW;
+      case PlacementPrice.HIGH:
+        return pin.offer.price > PriceScope.HIGH;
+      case PlacementPrice.MIDDLE:
+        return pin.offer.price >= PriceScope.LOW && pin.offer.price <= PriceScope.HIGH;
+      default:
+        return true;
+    }
+  };
+
   var filterPins = function () {
     window.pin.deletePins();
     window.card.removePopup();
@@ -25,7 +73,7 @@
   };
 
   var getFilteredPins = function () {
-    var filteredPins = pins.filter(filterPinsByHouseType);
+    var filteredPins = pins.filter(filterPinsByHouseType).filter(filterPinsByHousePrice).filter(filterPinsByHouseRoom).filter(filterPinsByHouseGuest);
 
     if (filteredPins.length > MAX_PINS_COUNT) {
       return filteredPins.slice(0, MAX_PINS_COUNT);
@@ -34,10 +82,10 @@
   };
 
   var addFormEvent = function () {
-    formContainer.addEventListener('change', function () {
+    formContainer.addEventListener('change',  window.debounce( function () {
       filterPins();
-    });
-  };
+    })
+  )};
 
   window.filter = {
     initialize: initialize
